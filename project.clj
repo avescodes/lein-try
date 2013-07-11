69b7d7812b3f0a64bf1a04dfeca0ffdb93936864
Ryan Neufeld <ryan@rkn.io>
2013-07-11 15:50:26 -0400

    Ship 0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-try "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject lein-try "0.1.0"
+  :description "Try out libraries without adding them as dependencies"
+  :url "https://github.com/rkneufeld/lein-try"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies  [[alembic  "0.1.3"]]
