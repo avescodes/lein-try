abdb314623fc6181c62e88b6e938dde1c5f88607
Ryan Neufeld <ryan@rkn.io>
2013-07-12 07:39:36 -0400

    Start 0.1.2 development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-try "0.1.1"
+(defproject lein-try "0.1.2-SNAPSHOT"
   :description "Try out libraries without adding them as dependencies"
   :url "https://github.com/rkneufeld/lein-try"
   :license {:name "Eclipse Public License"
