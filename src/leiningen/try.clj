131bd56e957b707ff4ce9c677693c337f6595426
Yannick Scherer <yannick.scherer@gmail.com>
2013-07-29 09:41:42 +0200

    Using 'RELEASE' if no version is given (see enhancement #7).

--- src/leiningen/try.clj
+++ src/leiningen/try.clj
@@ -3,22 +3,40 @@
             [leiningen.core.classpath :as lein-cp]
             [clojure.edn :as edn]))
 
-(defn ->dep-pairs
+(defn- version-string?
+  "Check if a given String represents a version number."
+  [^String s]
+  (or (contains? #{"RELEASE" "LATEST"} s)
+      (Character/isDigit (first s))))
+
+(def ->dep-pairs
   "From a sequence of command-line args describing dependency-version pairs,
-  return a list of vector pairs. Square braces in arg strings are ignored.
+   return a list of vector pairs. Square braces in arg strings are ignored. If
+   no version is given, 'RELEASE' will be used.
 
   Example:
   (->dep-pairs [\"clj-time\" \"\\\"0.5.1\\\"]\"])
   ; -> ([clj-time \"0.5.1\"])
 
   (->dep-pairs [\"[clj-time\" \"\\\"0.5.1\\\"]\"])
-  ; -> ([clj-time \"0.5.1\"])"
-  [args]
-  (->> args
-       (map #(clojure.string/replace % #"\[|\]" ""))
-       (partition 2)
-       (map vec)
-       (map #(update-in % [0] edn/read-string))))
+  ; -> ([clj-time \"0.5.1\"])
+  
+  (->dep-pairs [\"clj-time\" \"conformity\"])
+  ; -> ([clj-time \"RELEASE\"] [conformity \"RELEASE\"])"
+  (letfn [(lazy-convert [args]
+            (lazy-seq 
+              (when (seq args)
+                (let [[^String artifact & rst] args
+                      artifact (edn/read-string artifact)]
+                  (if-let [[^String v & nxt] (seq rst)]
+                    (if (version-string? v)
+                      (cons [artifact v] (lazy-convert nxt))
+                      (cons [artifact "RELEASE"] (lazy-convert rst)))
+                    (vector [artifact "RELEASE"]))))))]
+    (fn [args]
+      (->> args
+        (map #(clojure.string/replace % #"\[|\]" ""))
+        lazy-convert))))
 
 (defn- resolve-try-deps!
   "Resolve newly-added try-dependencies, adding them to classpath."
--- test/leiningen/try_test.clj
+++ test/leiningen/try_test.clj
@@ -3,10 +3,31 @@
             [leiningen.try :refer :all]))
 
 (deftest ->dep-pairs-test
-  (is (= '[[clj-time "0.5.1"]]
-         (->dep-pairs ["[clj-time" "0.5.1]"])))
-  (is (= '[[clj-time "0.5.1"] [http-kit "2.1.5"]]
-         (->dep-pairs ["[clj-time" "0.5.1]" "[http-kit" "2.1.5]"]))))
+  (testing "the explicit use of version numbers"
+    (is (= '[[clj-time "0.5.1"]]
+           (->dep-pairs ["[clj-time" "0.5.1]"])))
+    (is (= '[[clj-time "0.5.1"] [http-kit "2.1.5"]]
+           (->dep-pairs ["[clj-time" "0.5.1]" "[http-kit" "2.1.5]"]))))
+  (testing "the implicit use of 'RELEASE' if no version number is given"
+    (is (= '[[clj-time "RELEASE"] [http-kit "2.1.5"]]
+           (->dep-pairs ["clj-time" "[http-kit" "2.1.5]"])))
+    (is (= '[[clj-time "RELEASE"] [http-kit "2.1.5"]]
+           (->dep-pairs ["[clj-time]" "[http-kit" "2.1.5]"])))
+    (is (= '[[clj-time "RELEASE"] [http-kit "2.1.5"]]
+           (->dep-pairs ["[clj-time]" "http-kit" "2.1.5"])))
+    (is (= '[[clj-time "RELEASE"] [http-kit "2.1.5"]]
+           (->dep-pairs ["clj-time" "http-kit" "2.1.5"])))
+    (is (= '[[clj-time "RELEASE"] [http-kit "RELEASE"]]
+           (->dep-pairs ["clj-time" "http-kit"])))
+    (is (= '[[clj-time "0.5.1"] [http-kit "RELEASE"]]
+           (->dep-pairs ["clj-time" "0.5.1" "http-kit"]))))
+  (testing "the explicit use of 'RELEASE'"
+    (is (= '[[clj-time "RELEASE"] [http-kit "2.1.5"]]
+           (->dep-pairs ["[clj-time" "RELEASE]" "[http-kit" "2.1.5]"])))
+    (is (= '[[clj-time "RELEASE"] [http-kit "RELEASE"]]
+           (->dep-pairs ["[clj-time" "RELEASE]" "[http-kit" "RELEASE]"])))
+    (is (= '[[clj-time "RELEASE"] [http-kit "RELEASE"]]
+           (->dep-pairs ["clj-time" "http-kit" "RELEASE"])))))
 
 (deftest add-deps-test
   (let [add-try-deps #'leiningen.try/add-try-deps]
