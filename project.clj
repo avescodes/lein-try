2b11a3f443a1b288466be3b7b4ff97b47d33b6b1
Ryan Neufeld <ryan@rkn.io>
2013-09-13 10:36:37 -0400

    We depend on Clojure 1.5.1+

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/rkneufeld/lein-try"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies  []
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[leiningen "2.2.0"]]}}
   :eval-in-leiningen true)
