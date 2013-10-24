65e985ea649c02f4c4fa99113875396126f7d7a7
Ryan Neufeld <ryan@rkn.io>
2013-10-24 07:05:42 -0400

    Release 0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-try "0.4.0-SNAPSHOT"
+(defproject lein-try "0.4.0"
   :description "Try out libraries without adding them as dependencies"
   :url "https://github.com/rkneufeld/lein-try"
   :license {:name "Eclipse Public License"
